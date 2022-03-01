455e4e0133a172451ba0269db59415027fb82986
Hugo Vila <code.hvila@gmail.com>
2022-03-01 13:57:48 +0100

    Manage Component State Implementing State Reducer Pattern Using useReducer and useCallback

--- src/components/Wizard.js
+++ src/components/Wizard.js
@@ -1,18 +1,54 @@
-import { useState, createContext } from "react";
+import { useCallback, createContext, useReducer } from "react";
 import "./wizard.css";
 
 export const WizardContext = createContext();
 
 const Wizard = ({ children }) => {
-  const [activePage, setActivePage] = useState(0);
-  const [steps, setSteps] = useState(0);
+  const defaultInitialState = {
+    activePage: 0,
+    steps: 0,
+  };
+
+  const actions = {
+    NEXT_PAGE: "NEXT_PAGE",
+    PREV_PAGE: "PREV_PAGE",
+    SET_STEPS: "SET_STEPS",
+  };
+
+  const defaultReducer = (state, action) => {
+    const { activePage } = state;
+    switch (action.type) {
+      case actions.NEXT_PAGE:
+        return { ...state, activePage: activePage + 1 };
+
+      case actions.PREV_PAGE:
+        return { ...state, activePage: activePage - 1 };
+
+      case actions.SET_STEPS:
+        return { ...state, steps: action.payload };
+
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(defaultReducer, defaultInitialState);
 
   const handleGoPrevPageClick = () => {
-    setActivePage((index) => index - 1);
+    dispatch({ type: actions.PREV_PAGE });
   };
   const handleGoNextPageClick = () => {
-    setActivePage((index) => index + 1);
+    dispatch({ type: actions.NEXT_PAGE });
   };
+
+  const setSteps = useCallback(
+    (n) => {
+      dispatch({ type: actions.SET_STEPS, payload: n });
+    },
+    [actions.SET_STEPS, dispatch]
+  );
+
+  const { activePage, steps } = state;
 
   const context = {
     activePage,
